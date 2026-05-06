24fd0e534350cf53a04e3828b641cb1c1fb79694
Jeffrey Emanuel <jeff@jeffreyemanuel.com>
2026-05-05 21:20:31 -0400

    [mock-code-finder][tokio-quic-h3-unit-protocol-matrix] Remove contract suppressions

--- tests/tokio_quic_h3_unit_protocol_matrix.rs
+++ tests/tokio_quic_h3_unit_protocol_matrix.rs
@@ -1,5 +1,3 @@
-#![allow(warnings)]
-#![allow(clippy::all)]
 //! Contract tests for [T4.10] Exhaustive Unit/Protocol Test Matrix: QUIC and HTTP/3 State Machines.
 //!
 //! Bead: asupersync-2oh2u.4.10
