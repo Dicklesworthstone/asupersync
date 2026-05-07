8c089c8b79f5a12dacbb2b4b9276e60795c94240
Jeffrey Emanuel <jeff@jeffreyemanuel.com>
2026-05-06 21:24:36 -0400

    scripts: harden capacity envelope planner runner

--- scripts/run_capacity_envelope_planner_smoke.sh
+++ scripts/run_capacity_envelope_planner_smoke.sh
@@ -175,8 +175,32 @@ mkdir -p "$RUN_DIR"
 HOST_FINGERPRINT_JSON="$(host_fingerprint_json)"
 STARTED_TS="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
 
-printf -v RCH_INVOCATION '%q' "$RCH_BIN"
-COMMAND="${RCH_INVOCATION} exec -- env CARGO_INCREMENTAL=0 CARGO_TARGET_DIR=\${TMPDIR:-/tmp}/rch_target_capacity_envelope_planner ASUPERSYNC_CAPACITY_ENVELOPE_CONTRACT_PATH=${ARTIFACT} ASUPERSYNC_CAPACITY_ENVELOPE_SCENARIO=${SCENARIO} ASUPERSYNC_CAPACITY_ENVELOPE_REPORT_PATH=${SCENARIO_REPORT_PATH} cargo test -p asupersync --test capacity_envelope_planner_contract capacity_envelope_smoke_contract_emits_report --features test-internals -- --nocapture"
+COMMAND_ARGS=(
+    "$RCH_BIN"
+    exec
+    --
+    env
+    "CARGO_INCREMENTAL=0"
+    "CARGO_PROFILE_TEST_DEBUG=0"
+    "RUSTFLAGS=-D warnings -C debuginfo=0"
+    "CARGO_TARGET_DIR=${TMPDIR:-/tmp}/rch_target_capacity_envelope_planner"
+    "ASUPERSYNC_CAPACITY_ENVELOPE_CONTRACT_PATH=${ARTIFACT}"
+    "ASUPERSYNC_CAPACITY_ENVELOPE_SCENARIO=${SCENARIO}"
+    "ASUPERSYNC_CAPACITY_ENVELOPE_REPORT_PATH=${SCENARIO_REPORT_PATH}"
+    cargo
+    test
+    -p
+    asupersync
+    --test
+    capacity_envelope_planner_contract
+    capacity_envelope_smoke_contract_emits_report
+    --features
+    test-internals
+    --
+    --nocapture
+)
+printf -v COMMAND '%q ' "${COMMAND_ARGS[@]}"
+COMMAND="${COMMAND% }"
 
 COMMAND_EXIT_CODE=0
 SCRIPT_EXIT_CODE=0
@@ -195,7 +219,10 @@ else
     COMMAND_EXIT_CODE=-1
     EARLY_SUCCESS=0
     set +e
-    bash -lc "$COMMAND" >"$RUN_LOG_PATH" 2>&1 &
+    (
+        cd "$PROJECT_ROOT"
+        "${COMMAND_ARGS[@]}"
+    ) >"$RUN_LOG_PATH" 2>&1 &
     COMMAND_PID=$!
     set -e
 
