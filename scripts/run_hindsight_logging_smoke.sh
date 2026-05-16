766c40d40cc92765406cbb45117c70e8358a3461
Jeffrey Emanuel <jeff@jeffreyemanuel.com>
2026-05-16 05:47:05 -0400

    [br-asupersync-lhx6m4] Keep hindsight logging smoke cargo behind rch

--- scripts/run_hindsight_logging_smoke.sh
+++ scripts/run_hindsight_logging_smoke.sh
@@ -100,7 +100,7 @@ COMMAND_ARGS=(
   "CARGO_PROFILE_TEST_DEBUG=0"
   "RUSTFLAGS=-D warnings -C debuginfo=0"
   "CARGO_TARGET_DIR=${TMPDIR:-/tmp}/rch_target_hindsight_logging"
-  cargo
+  "${CARGO_BIN:-cargo}"
   test
   -p
   asupersync
