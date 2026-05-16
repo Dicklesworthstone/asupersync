fff3a64ffa3ec3681a728bba663213bcacb642f5
Jeffrey Emanuel <jeff@jeffreyemanuel.com>
2026-05-16 04:01:15 -0400

    [br-asupersync-lhx6m4] Keep digital twin smoke cargo behind rch

--- scripts/run_digital_twin_smoke.sh
+++ scripts/run_digital_twin_smoke.sh
@@ -113,7 +113,7 @@ run_scenario() {
         CARGO_PROFILE_TEST_DEBUG=0
         "RUSTFLAGS=-C debuginfo=0"
         "CARGO_TARGET_DIR=${TMPDIR:-/tmp}/rch_target_digital_twin_${safe_sid}"
-        cargo test -p asupersync --test digital_twin_contract --features test-internals
+        "${CARGO_BIN:-cargo}" test -p asupersync --test digital_twin_contract --features test-internals
     )
     if [[ -n "$test_filter" ]]; then
         command+=("$test_filter")
