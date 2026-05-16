9f21e173ba66d1403c0462635ecca8706c570ffb
Jeffrey Emanuel <jeff@jeffreyemanuel.com>
2026-05-16 06:42:47 -0400

    [br-asupersync-lhx6m4] Keep replay minimization smoke cargo behind rch

--- scripts/run_replay_minimization_validation_smoke.sh
+++ scripts/run_replay_minimization_validation_smoke.sh
@@ -103,7 +103,7 @@ COMMAND_ARGS=(
   "CARGO_PROFILE_TEST_DEBUG=0"
   "RUSTFLAGS=-D warnings -C debuginfo=0"
   "CARGO_TARGET_DIR=${TMPDIR:-/tmp}/rch_target_replay_minimization_validation"
-  cargo
+  "${CARGO_BIN:-cargo}"
   test
   -p
   asupersync
